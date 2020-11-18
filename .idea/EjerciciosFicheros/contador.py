02527e94f78c7a8950cebdb487c2d302171ad11b
Alvaro Merino Fernandez <alvaro.merino.fernandez@gmail.com>
2020-11-18 09:45:55 +0100

    Ejercicio 2 resuelto (primera prueba)

--- .idea/EjerciciosFicheros/contador.py
+++ .idea/EjerciciosFicheros/contador.py
@@ -1,6 +1,32 @@
-with open("personas.txt","r",encoding="utf8") as file:
-    personas=file.readlines()
-for persona in personas:
-    datos=persona.split(";")
-    print(f"id:{datos[0]}, nombre y apellidos: {datos[1]} {datos[2]}, fecha de nacimiento: {datos[3]}",end="")
+from io import open
+import sys
 
+fichero = open("contador.txt", "a+")
+fichero.seek(0)
+contenido = fichero.readline()
+
+if len(contenido) == 0:
+    contenido = "0"
+    fichero.write(contenido)
+
+fichero.close()
+
+# Vamos a intentar transformar el texto a un número
+try:
+    contador: int = int(contenido)
+
+    # En función de lo que el usuario quiera...
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "inc":
+            contador += 1
+        elif sys.argv[1] == "dec":
+            contador -= 1
+
+    print(contador)
+    # Finalmente volvemos a escribir los cambios en el fichero
+    fichero = open("contador.txt", "w")
+    fichero.write( str(contador) )
+    fichero.close()
+
+except:
+    print("Error: Fichero corrupto.")
--- .idea/EjerciciosFicheros/personas.py
+++ .idea/EjerciciosFicheros/personas.py
@@ -2,5 +2,5 @@ with open("personas.txt","r",encoding="utf8") as file:
     personas=file.readlines()
 for persona in personas:
     datos=persona.split(";")
-    print(f"id:{datos[0]}, nombre y apellidos: {datos[1]} {datos[2]}, fecha de nacimiento: {datos[3]}",end="")
+    print(f"id:{datos[0]}, nombre y apellidos: {datos[1]} {datos[2]}, fecha de nacimiento: {datos[3]}")#,end="")
 
